450be267917eda5de917d0ddc8cc7cf784e8b2eb
mr-mixas <mixas.sr@gmail.com>
2016-08-09 23:09:51 +0300

    refactoring

--- t/_common.pm
+++ t/_common.pm
@@ -28,9 +28,10 @@ our $s_mixed = {
 };
 
 sub scmp($$$) { # compare structures by data
-    my $got = Data::Dumper->new([shift])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Deepcopy(1)->Dump();
-    my $exp = Data::Dumper->new([shift])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Deepcopy(1)->Dump();
-    print STDERR "\nDEBUG: === " . shift . " ===\ngot: $got\nexp: $exp\n" if ($ENV{DEBUG});
+    my ($got, $exp, $txt) = @_;
+    $got = Data::Dumper->new([$got])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Deepcopy(1)->Dump();
+    $exp = Data::Dumper->new([$exp])->Terse(1)->Sortkeys(1)->Quotekeys(0)->Indent(0)->Deepcopy(1)->Dump();
+    print STDERR "\nDEBUG: === $txt ===\ngot: $got\nexp: $exp\n" if ($ENV{DEBUG});
     return $got eq $exp;
 }
 
